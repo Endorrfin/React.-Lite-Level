b37977ce30eac83326521af88e7979ef12f954d8
Vasyl Krupka <krupka.ua@gmail.com>
2019-11-07 19:36:05 +0200

    updated lesson 6 - react-lite-level-06-components

---
+++ react-lite-level-06-components/src/footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Footer.css';
+
+
+ function App() {
+  return (
+    <footer className="section footer-classic context-dark bg-image" style={{background: "#2d3246", color: "#fff"}}>
+          <p>Copyright</p>
+        </footer>
+  )
+ }
+
+ export default App;
---
+++ react-lite-level-06-components/src/main/Main.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Main.css';
+
+
+ class Main extends React.Component {
+   render() {
+    return (
+      <div className="container">
+      <main>
+          <article>
+            <header>
+              <h1>Article 1</h1>
+              <h2>Article 2</h2>
+            </header>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus doloribus sunt laboriosam dolore ad enim nulla in blanditiis. Nihil eius corrupti error minima rem amet, officia esse? Molestiae tempore quam natus neque velit fugiat ipsam hic? Rem earum libero alias autem, velit laboriosam ipsam numquam quod non? Atque incidunt non quam inventore. Eius amet vitae sed architecto. A aliquid animi unde accusamus, corrupti ullam molestias doloribus nihil vel, natus dolorem odit dolorum ipsa nesciunt facere nulla officia corporis saepe quidem, laudantium asperiores? Laudantium temporibus ex distinctio exercitationem autem mollitia rerum magnam recusandae veniam quidem, ea veritatis, accusamus nemo nam hic!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus doloribus sunt laboriosam dolore ad enim nulla in blanditiis. Nihil eius corrupti error minima rem amet, officia esse? Molestiae tempore quam natus neque velit fugiat ipsam hic? Rem earum libero alias autem, velit laboriosam ipsam numquam quod non? Atque incidunt non quam inventore. Eius amet vitae sed architecto. A aliquid animi unde accusamus, corrupti ullam molestias doloribus nihil vel, natus dolorem odit dolorum ipsa nesciunt facere nulla officia corporis saepe quidem, laudantium asperiores? Laudantium temporibus ex distinctio exercitationem autem mollitia rerum magnam recusandae veniam quidem, ea veritatis, accusamus nemo nam hic!</p>
+          </article>
+        </main>
+        </div>
+    )
+  }
+}
+
+ export default Main;
